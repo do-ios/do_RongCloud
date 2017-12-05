d74096d31680cfc079eac48d522611defec2b34d
zmj <928217176@qq.com>
2017-12-05 10:19:11 +0800

    文件功能;UI Bug修复

--- doExtLib/DoRCConversationViewController.m
+++ doExtLib/DoRCConversationViewController.m
@@ -10,8 +10,10 @@
 #import "UIColor+DORCColor.h"
 #import "doInvokeResult.h"
 #import "do_RongUtil.h"
-@interface DoRCConversationViewController ()
+#import <RongIMKit/RongIMKit.h>
 
+@interface DoRCConversationViewController ()
+@property (nonatomic, assign) BOOL navigationBarHidden;
 @end
 
 @implementation DoRCConversationViewController
@@ -19,21 +21,23 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.navigationController setNavigationBarHidden:false animated:false];
+    _navigationBarHidden = true;
     
 }
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    [self.navigationController setNavigationBarHidden:true animated:false];
+    [self.navigationController setNavigationBarHidden:_navigationBarHidden animated:false];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _navigationBarHidden = true;
     // Do any additional setup after loading the view.
     UIView *fillView = [[UIView alloc] initWithFrame:CGRectMake(0, -20,[UIScreen mainScreen].bounds.size.width, 20)];
     fillView.backgroundColor = self.titleBarColor ? self.titleBarColor : [UIColor blueColor];
     [self.navigationController.navigationBar addSubview:fillView];
     
-//    self.automaticallyAdjustsScrollViewInsets = YES;
+    //    self.automaticallyAdjustsScrollViewInsets = YES;
     // 导航栏背景色
     [self.navigationController.navigationBar setBackgroundImage:[UIColor imageWithColor:[UIColor clearColor]] forBarMetrics:UIBarMetricsDefault];
     UIColor *bgColor = self.titleBarColor ? self.titleBarColor : [UIColor blueColor];
@@ -51,6 +55,16 @@
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:backImg style:UIBarButtonItemStylePlain target:self action:@selector(back)];
     if (self.targetUserName != nil) {
         self.navigationItem.title = self.targetUserName;
+    }
+    
+    if (self.conversationType != ConversationType_APPSERVICE &&
+        self.conversationType != ConversationType_PUBLICSERVICE) {
+        //加号区域增加发送文件功能，Kit中已经默认实现了该功能，但是为了SDK向后兼容性，目前SDK默认不开启该入口，可以参考以下代码在加号区域中增加发送文件功能。
+        UIImage *imageFile = [RCKitUtility imageNamed:@"actionbar_file_icon" ofBundle:@"RongCloud.bundle"];
+        [self.chatSessionInputBarControl.pluginBoardView insertItemWithImage:imageFile
+                                                                       title:NSLocalizedStringFromTable(@"File", @"RongCloudKit", nil)
+                                                                     atIndex:3
+                                                                         tag:PLUGIN_BOARD_ITEM_FILE_TAG];
     }
 }
 
@@ -86,6 +100,13 @@
     NSString *conversationType = [[do_RongUtil shareInstance] conversationTypeStringWithRCConversationType:self.conversationType];
     [result SetResultNode:@{@"conversationType":conversationType,@"userId":userId,@"userName":userName}];
     [self.eventCenter FireEvent:@"userPortraitClick" :result];
+}
+
+- (void)didTapMessageCell:(RCMessageModel *)model {
+    if ([model.objectName isEqualToString:@"RC:FileMsg"]) { // 即将跳转查看文件页面
+        _navigationBarHidden = false;
+    }
+    [super didTapMessageCell:model];
 }
 
 
